730b0f8f0b03b6034c11ed32420671a183e41450
PurpleRanger <60563337+cppranger@users.noreply.github.com>
2021-03-29 15:07:53 +0300

    Add files via upload

---
+++ src/main/java/ru/shoplist/persist/ShoppingItem.java
@@ -0,0 +1,32 @@
+package ru.shoplist.persist;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "shopping_items")
+public class ShoppingItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public ShoppingItem() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ru/shoplist/persist/ShoppingItemRepository.java
@@ -0,0 +1,9 @@
+package ru.shoplist.persist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShoppingItemRepository extends JpaRepository<ShoppingItem, Long> {
+
+}
